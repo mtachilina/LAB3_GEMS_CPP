bd7e8a76294f34681d9f99c001f286745d98e1b6
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-06-11 16:09:45 +0300

    Update ColorGem.cpp

--- ColorGem.cpp
+++ ColorGem.cpp
@@ -1,12 +1,20 @@
-#pragma once
-#include "Gem.hpp"
+#include "ColorGem.hpp"
+#include "Board.hpp"
 
-class ColorGem : public Gem {
-public:
-    using Gem::Gem;
+void ColorGem::activate(Board& board, int i, int j) {
+    int savedColor = getColor();
+    board.activateColor(i, j, savedColor);
+}
 
-    bool isBonus() const override { return true; }
-    void activate(Board& board, int i, int j) override;
+void ColorGem::draw(sf::RenderWindow& window, const sf::Color colors[], int cellSize, float offsetX, float offsetY) {
 
-    void draw(sf::RenderWindow& window, const sf::Color colors[], int cellSize, float offsetX, float offsetY);
-};
+    sf::RectangleShape shape(sf::Vector2f(cellSize - 2.f, cellSize - 2.f));
+    shape.setPosition(offsetX, offsetY);
+    shape.setFillColor(colors[color]);
+    window.draw(shape);
+
+    sf::CircleShape dot(cellSize / 4.f);
+    dot.setPosition(offsetX + cellSize / 4.f, offsetY + cellSize / 4.f);
+    dot.setFillColor(sf::Color::White);
+    window.draw(dot);
+}
