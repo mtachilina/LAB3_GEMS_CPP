a383e4abee8cb27142613c952419aee1edba97dc
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-06-11 16:27:44 +0300

    Update BombGem.hpp

--- BombGem.hpp
+++ BombGem.hpp
@@ -6,7 +6,7 @@ public:
     using Gem::Gem;
 
     bool isBonus() const override { return true; }
-    void activate(Board& board, int i, int j);
+    void activate(Board& board, int i, int j) override;
 
     void draw(sf::RenderWindow& window,
         const sf::Color colors[],
