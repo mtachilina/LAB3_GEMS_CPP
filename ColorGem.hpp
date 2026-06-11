87efa77d511b195fd752b10b8e9bcaa31394e534
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-06-11 16:21:52 +0300

    Update ColorGem.hpp

--- ColorGem.hpp
+++ ColorGem.hpp
@@ -6,6 +6,7 @@ public:
     using Gem::Gem;
 
     bool isBonus() const override { return true; }
+    void activate(Board& board, int i, int j) override;
 
     void draw(sf::RenderWindow& window, const sf::Color colors[], int cellSize, float offsetX, float offsetY);
 };
